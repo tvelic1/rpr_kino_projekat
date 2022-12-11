895db4e70e56dd49fac95b26bd814ed2b1207111
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-11 21:35:44 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/domain/GLEDATELJI.java
+++ src/main/java/ba/unsa/etf/rpr/domain/GLEDATELJI.java
@@ -1,4 +1,55 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class GLEDATELJI {
+    private int jmbg;
+    private String ime;
+    private filmovi id_film;
+
+    public int getJmbg() {
+        return jmbg;
+    }
+
+    public void setJmbg(int jmbg) {
+        this.jmbg = jmbg;
+    }
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public filmovi getId_film() {
+        return id_film;
+    }
+
+    public void setId_film(filmovi id_film) {
+        this.id_film = id_film;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof GLEDATELJI)) return false;
+        GLEDATELJI that = (GLEDATELJI) o;
+        return getJmbg() == that.getJmbg() && getIme().equals(that.getIme()) && getId_film().equals(that.getId_film());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getJmbg(), getIme(), getId_film());
+    }
+
+    @Override
+    public String toString() {
+        return "GLEDATELJI{" +
+                "jmbg=" + jmbg +
+                ", ime='" + ime + '\'' +
+                ", id_film=" + id_film +
+                '}';
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/domain/vrstafilma.java
+++ src/main/java/ba/unsa/etf/rpr/domain/vrstafilma.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr.domain;
 
+import java.util.Objects;
+
 public class vrstafilma {
+
+    private int id;
+    private String zanr;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getZanr() {
+        return zanr;
+    }
+
+    public void setZanr(String zanr) {
+        this.zanr = zanr;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof vrstafilma)) return false;
+        vrstafilma that = (vrstafilma) o;
+        return getId() == that.getId() && getZanr().equals(that.getZanr());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getZanr());
+    }
+
+    @Override
+    public String toString() {
+        return "vrstafilma{" +
+                "id=" + id +
+                ", zanr='" + zanr + '\'' +
+                '}';
+    }
 }
