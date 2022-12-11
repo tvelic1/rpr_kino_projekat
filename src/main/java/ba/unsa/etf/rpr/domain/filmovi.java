a54cf0b636bfebd598235293568a91991756d9b7
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-11 21:27:27 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/domain/filmovi.java
+++ src/main/java/ba/unsa/etf/rpr/domain/filmovi.java
@@ -7,46 +7,46 @@ public class filmovi {
 
     private int trajanje;
     private String ime;
-    private int id_vrsta_filma;
-
-    public void setIdfilma(int idfilma) {
-        this.idfilma = idfilma;
-    }
-
-    public void setOcjena(String ocjena) {
-        this.ocjena = ocjena;
-    }
-
-    public void setTrajanje(int trajanje) {
-        this.trajanje = trajanje;
-    }
-
-    public void setIme(String ime) {
-        this.ime = ime;
-    }
-
-    public void setId_vrsta_filma(int id_vrsta_filma) {
-        this.id_vrsta_filma = id_vrsta_filma;
-    }
+    private vrstafilma id_vrsta_filma;
 
     public int getIdfilma() {
         return idfilma;
+    }
+
+    public void setIdfilma(int idfilma) {
+        this.idfilma = idfilma;
     }
 
     public String getOcjena() {
         return ocjena;
     }
 
+    public void setOcjena(String ocjena) {
+        this.ocjena = ocjena;
+    }
+
     public int getTrajanje() {
         return trajanje;
+    }
+
+    public void setTrajanje(int trajanje) {
+        this.trajanje = trajanje;
     }
 
     public String getIme() {
         return ime;
     }
 
-    public int getId_vrsta_filma() {
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public vrstafilma getId_vrsta_filma() {
         return id_vrsta_filma;
+    }
+
+    public void setId_vrsta_filma(vrstafilma id_vrsta_filma) {
+        this.id_vrsta_filma = id_vrsta_filma;
     }
 
     @Override
@@ -54,7 +54,7 @@ public class filmovi {
         if (this == o) return true;
         if (!(o instanceof filmovi)) return false;
         filmovi filmovi = (filmovi) o;
-        return getIdfilma() == filmovi.getIdfilma() && getTrajanje() == filmovi.getTrajanje() && getId_vrsta_filma() == filmovi.getId_vrsta_filma() && getOcjena().equals(filmovi.getOcjena()) && getIme().equals(filmovi.getIme());
+        return getIdfilma() == filmovi.getIdfilma() && getTrajanje() == filmovi.getTrajanje() && getOcjena().equals(filmovi.getOcjena()) && getIme().equals(filmovi.getIme()) && getId_vrsta_filma().equals(filmovi.getId_vrsta_filma());
     }
 
     @Override
