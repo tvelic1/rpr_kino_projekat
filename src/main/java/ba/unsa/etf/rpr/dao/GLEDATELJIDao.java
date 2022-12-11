b44bee4723f05cfcdd0950201198d257f752cbae
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-11 22:07:28 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/dao/GLEDATELJIDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/GLEDATELJIDao.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr.dao;
+import ba.unsa.etf.rpr.domain.GLEDATELJI;
+import java.util.List;
 
-public class GLEDATELJIDao {
+public interface GLEDATELJIDao extends Dao<GLEDATELJI> {
+    List<GLEDATELJI> traziPoId(int id1,int id2);
+    //List<GLEDATELJI> trazipoImenu(String pocetak,String kraj);
 }
--- src/main/java/ba/unsa/etf/rpr/dao/filmoviDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/filmoviDao.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rpr.dao;
-
-public class filmoviDao {
+import ba.unsa.etf.rpr.domain.vrstafilma;
+import ba.unsa.etf.rpr.domain.filmovi;
+import java.util.List;
+public interface filmoviDao extends Dao<filmovi>{
+    List<filmovi> searchByName(String name);
+    List<filmovi> searchByZanr(vrstafilma zanr);
 }
