d1016057aaed4619cc270c46d42b23c764080b3a
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-15 22:22:24 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/AbstractDao.java
@@ -67,4 +67,88 @@ public  abstract  class AbstractDao<T extends Idable> implements Dao<T> {
         }
     }
 
+    public void delete(int id) throws filmoviException{
+        String q="DELETE FROM "+tableName+" WHERE id=?";
+        try{
+            PreparedStatement st=getCon().prepareStatement(q,Statement.RETURN_GENERATED_KEYS);
+            st.setObject(1,id);
+            st.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new filmoviException(e.getMessage(),e);
+        }
+    }
+    private String prepareUpdateParts(Map<String,Object>row){
+        StringBuilder s=new StringBuilder();
+        int brojac=0;
+        for(Map.Entry<String,Object>entry:row.entrySet()){
+            brojac++;
+            if(entry.getKey().equals("id") || entry.getKey().equals("idfilma") || entry.getKey().equals("jmbg")) continue;
+            s.append(entry.getKey()).append("=?");
+            if(row.size()!=brojac);
+            s.append(",");
+
+    } return s.toString();}
+
+    private  Map.Entry<String,String> prepareInsertParts(Map<String,Object> row){
+        StringBuilder s=new StringBuilder();
+        StringBuilder q=new StringBuilder();
+        int b=0;
+        for(Map.Entry<String,Object> entry: row.entrySet()){
+            b++;
+            if(entry.getKey().equals("id") || entry.getKey().equals("idfilma") || entry.getKey().equals("jmbg")  ) continue;
+            s.append(entry.getKey());
+            q.append("?");
+            if(row.size()!=b){
+                s.append(","); q.append(",");
+            }
+        } return new AbstractMap.SimpleEntry<String,String>(s.toString(),q.toString());
+    }
+
+    public T add(T item) throws filmoviException{
+        Map<String,Object> row=object2row(item);
+        Map.Entry<String,String> c=prepareInsertParts(row);
+        StringBuilder s=new StringBuilder();
+        s.append("INSERT INTO ").append(tableName);
+        s.append(" (").append(c.getKey()).append(") ");
+        s.append("VALUES (").append(c.getValue()).append(") ");
+        try{
+            PreparedStatement st= getCon().prepareStatement(s.toString(),Statement.RETURN_GENERATED_KEYS);
+            int br=1;
+            for(Map.Entry<String,Object> entry: row.entrySet()){
+                if(entry.getKey().equals("id") || entry.getKey().equals("idfilma") || entry.getKey().equals("jmbg")) continue;
+                st.setObject(br,entry.getValue());
+                br++;
+            }
+            st.executeUpdate();
+            ResultSet r=st.getGeneratedKeys();
+            r.next();
+            item.setId(r.getInt(1));
+            return item;
+        } catch (SQLException e) {
+            throw new filmoviException(e.getMessage(),e);
+        }
+    }
+    public T update(T item ) throws filmoviException{
+        Map<String,Object>row=object2row(item);
+        String c=prepareUpdateParts(row);
+        StringBuilder s=new StringBuilder();
+        s.append("UPDATE ").append(tableName).append(" SET ").append(c).append(" WHERE id=?");
+        try{
+            PreparedStatement st= getCon().prepareStatement(s.toString(),Statement.RETURN_GENERATED_KEYS);
+            int br=1;
+            for(Map.Entry<String,Object> entry: row.entrySet()){
+                if(entry.getKey().equals("id") || entry.getKey().equals("idfilma") || entry.getKey().equals("jmbg")) continue;
+                st.setObject(br,entry.getValue());
+                br++;
+            }
+            st.setObject(br+1,item.getId());
+            st.executeUpdate();
+            return item;
+        } catch (SQLException e) {
+            throw new filmoviException(e.getMessage(),e);
+        }
+
+    }
+
 }
