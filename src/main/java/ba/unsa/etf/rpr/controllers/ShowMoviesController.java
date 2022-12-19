24747a5b9ee3935aaecd38b5fda64c7716c8de37
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-19 12:30:46 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/controllers/ShowMoviesController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/ShowMoviesController.java
@@ -1,4 +1,20 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.dao.vrstafilmaDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.vrstafilma;
+import ba.unsa.etf.rpr.exceptions.filmoviException;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class ShowMoviesController {
+    public Button okbtn;
+    public TextField text;
+    public vrstafilma vrsta;
+    public vrstafilmaDaoSQLImpl dao = new vrstafilmaDaoSQLImpl();
+
+    public void OkbttnAction(ActionEvent actionEvent){
+        vrsta=new vrstafilma();
+        vrsta.setZanr(text.getText());
+    }
 }
