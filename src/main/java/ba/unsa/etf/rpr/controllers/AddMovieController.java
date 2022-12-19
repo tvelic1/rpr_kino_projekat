c144d1a35d19342a5d69dec6eed81b1ec154b250
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-19 12:58:39 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
@@ -1,4 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
+import ba.unsa.etf.rpr.dao.filmoviDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.vrstafilma;
+import ba.unsa.etf.rpr.domain.filmovi;
+import ba.unsa.etf.rpr.exceptions.filmoviException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.util.List;
 
 public class AddMovieController {
+    public ChoiceBox<vrstafilma> choiceCatId;
+    public TextField fldAddQuote;
+    public DatePicker fldDatePickerId;
+    private ObservableList<vrstafilma> observableList;
+    private filmoviDaoSQLImpl filmDaoSQL;
+    public AddMovieController(List<vrstafilma> categoryList, filmoviDaoSQLImpl filmDaoSQL) {
+        observableList = FXCollections.observableArrayList(categoryList);
+        this.filmDaoSQL = filmDaoSQL;
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.filmoviDaoSQLImpl;
+import ba.unsa.etf.rpr.domain.vrstafilma;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+import java.util.List;
+
 public class HomeController {
+
 }
