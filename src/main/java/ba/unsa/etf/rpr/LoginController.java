bfe2ac977a2b115917f0464ea0dd8bdb63bd2ac4
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-19 11:03:22 +0100

    .

---
+++ src/main/java/ba/unsa/etf/rpr/LoginController.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr;
+
+import javafx.event.ActionEvent;
+
+public class LoginController {
+    public void login(ActionEvent actionEvent){}
+}
---
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -0,0 +1,23 @@
+package ba.unsa.etf.rpr;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainFX extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        System.out.println(getClass());
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
+        stage.setTitle("User Login");
+        stage.setScene(new Scene(root, 800, 500));
+        stage.show();
+
+    }
+    public static void main(String[] args){
+
+        launch(args);
+    }
+}
