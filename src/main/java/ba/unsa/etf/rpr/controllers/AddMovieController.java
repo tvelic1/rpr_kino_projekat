8293b67170d93fd14c706048c59cecfe75280f2e
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-19 13:07:18 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/AddMovieController.java
@@ -16,11 +16,30 @@ import java.util.List;
 public class AddMovieController {
     public ChoiceBox<vrstafilma> choiceCatId;
     public TextField fldAddQuote;
-    public DatePicker fldDatePickerId;
+   // public DatePicker fldDatePickerId;
     private ObservableList<vrstafilma> observableList;
     private filmoviDaoSQLImpl filmDaoSQL;
-    public AddMovieController(List<vrstafilma> categoryList, filmoviDaoSQLImpl filmDaoSQL) {
+    public AddMovieController(List<vrstafilma> categoryList, filmoviDaoSQLImpl filmDaoSQL){
         observableList = FXCollections.observableArrayList(categoryList);
         this.filmDaoSQL = filmDaoSQL;
+    }
+
+    @FXML
+    public void initialize() {
+        choiceCatId.setItems(observableList);
+    }
+    public void onActionAddMovie(ActionEvent actionEvent) {
+        filmovi newFilm = new filmovi();
+        try {
+            newFilm.setId_vrsta_filma(choiceCatId.getSelectionModel().getSelectedItem());
+            newFilm.setIme(fldAddQuote.getText());
+
+            filmDaoSQL.add(newFilm);
+            Stage stage = (Stage) fldAddQuote.getScene().getWindow();
+            stage.close();
+        } catch (filmoviException e) {
+            System.out.println("Something went wrong in onActionAddMovie method");
+            throw new RuntimeException(e);
+        }
     }
 }
