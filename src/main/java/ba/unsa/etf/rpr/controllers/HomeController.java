6a051170fbf44197a5ecd08bad57a2af66638e09
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-28 00:56:46 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/HomeController.java
@@ -1,15 +1,26 @@
 package ba.unsa.etf.rpr.controllers;
 
+import ba.unsa.etf.rpr.dao.vrstafilmaDao;
+import ba.unsa.etf.rpr.dao.vrstafilmaDaoSQLImpl;
 import ba.unsa.etf.rpr.dao.filmoviDaoSQLImpl;
 import ba.unsa.etf.rpr.domain.vrstafilma;
+import ba.unsa.etf.rpr.domain.filmovi;
+import ba.unsa.etf.rpr.exceptions.filmoviException;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.TextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-import java.util.List;
+import java.io.IOException;
 
-public class HomeController {
+import static javafx.scene.layout.Region.USE_COMPUTED_SIZE;
+public class HomeController { int a;
 
 }
