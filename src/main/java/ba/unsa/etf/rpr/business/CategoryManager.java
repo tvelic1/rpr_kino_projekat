38b6069b5af0c17ccd914222c2694feb1f54c97b
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-29 14:39:24 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.exceptions.filmoviException;
 
 public class CategoryManager {
+    public void validateCategoryName(String name ) throws filmoviException{
+        if(name == null || name.length()>45 || name.length()<3){
+            throw new filmoviException("Kategorija mora imati izmedju 3 i 45 slova.");
+        }
+
+    }
+
 }
