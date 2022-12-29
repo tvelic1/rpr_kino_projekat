3f86265a917d2549d00918717462694dc53ac311
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-29 14:45:51 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/CategoryManager.java
@@ -1,5 +1,7 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.DaoFactory;
 import ba.unsa.etf.rpr.exceptions.filmoviException;
+import ba.unsa.etf.rpr.domain.vrstafilma;
 
 public class CategoryManager {
     public void validateCategoryName(String name ) throws filmoviException{
@@ -8,5 +10,19 @@ public class CategoryManager {
         }
 
     }
+public vrstafilma add(vrstafilma v) throws filmoviException{
+        if(v.getId()!=0){
+            throw new filmoviException("Ne možete dodati kategorije preko ID-a, ID se automatski dodjeljuje");
+        }validateCategoryName(v.getZanr());
+
+        try{
+            return DaoFactory.vrstaaDao().add(v);
+        }catch(filmoviException e){
+            if(e.getMessage().contains("UQ_NAME"))
+                throw new filmoviException("Već postoji taj žanr");
+            throw e;
+        }
+
+        }
 
 }
