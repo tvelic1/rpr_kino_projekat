ff15b57a7f1c3306d3dfb4e4d7f6f35c53949b11
tvelic1 <tvelic1@etf.unsa.ba>
2022-12-29 14:58:26 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/business/FilmoviManager.java
+++ src/main/java/ba/unsa/etf/rpr/business/FilmoviManager.java
@@ -1,4 +1,21 @@
 package ba.unsa.etf.rpr.business;
+import ba.unsa.etf.rpr.dao.Dao;
+import ba.unsa.etf.rpr.dao.DaoFactory;
+import ba.unsa.etf.rpr.domain.filmovi;
+import ba.unsa.etf.rpr.exceptions.filmoviException;
 
-public class FilmoviManager {
+import java.util.List;
+
+
+public class FilmoviManager  {
+    public List<filmovi>getAll() throws filmoviException{
+        return DaoFactory.filmDao().getAll();
+    }
+    public List<filmovi>search(String text)throws filmoviException{
+        return DaoFactory.filmDao().searchByName(text);
+    }
+    public void delete (int id) throws filmoviException{
+        DaoFactory.filmDao().delete(id);
+
+    }
 }
