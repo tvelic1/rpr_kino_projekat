3d42506382de434be89977c30d55ceaef4dcc5f7
tvelic1 <tvelic1@etf.unsa.ba>
2023-01-05 17:58:12 +0100

    registracija gotova

--- src/main/java/ba/unsa/etf/rpr/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/LoginController.java
@@ -1,19 +1,33 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import java.io.IOException;
 
 public class LoginController {
+    public Button submitButton;
     private Stage stage;
     private Scene scene;
     private Parent root;
-    public void login(ActionEvent actionEvent){}
+    @FXML
+    private PasswordField pass;
+    @FXML
+    private TextField email;
+
+    public void login(ActionEvent actionEvent){
+        Window owner=submitButton.getScene().getWindow();
+
+    }
     public void register(ActionEvent event) throws IOException {
         root = FXMLLoader.load(getClass().getResource("/register.fxml"));
         stage=(Stage)((Node)event.getSource()).getScene().getWindow();
