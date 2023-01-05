927f14b1fa50fd455f6e196a8ac95b0a507f23b8
tvelic1 <tvelic1@etf.unsa.ba>
2023-01-05 17:18:21 +0100

    registracija gotova

--- src/main/java/ba/unsa/etf/rpr/LoginController.java
+++ src/main/java/ba/unsa/etf/rpr/LoginController.java
@@ -1,7 +1,25 @@
 package ba.unsa.etf.rpr;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     public void login(ActionEvent actionEvent){}
+    public void register(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/register.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/MainFX.java
+++ src/main/java/ba/unsa/etf/rpr/MainFX.java
@@ -6,13 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import static javafx.scene.control.PopupControl.USE_COMPUTED_SIZE;
+
 public class MainFX extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         System.out.println(getClass());
-        Parent root = FXMLLoader.load(getClass().getResource("/register.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/login.fxml"));
         stage.setTitle("User Login");
-        stage.setScene(new Scene(root, 800, 500));
+        stage.setScene(new Scene(root, USE_COMPUTED_SIZE, USE_COMPUTED_SIZE));
         stage.show();
 
     }
