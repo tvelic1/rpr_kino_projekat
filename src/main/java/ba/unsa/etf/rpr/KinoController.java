f6c46c849f7a3cd87374312e84266f0511464550
tvelic1 <tvelic1@etf.unsa.ba>
2023-01-17 22:46:50 +0100

    .

--- src/main/java/ba/unsa/etf/rpr/KinoController.java
+++ src/main/java/ba/unsa/etf/rpr/KinoController.java
@@ -1,4 +1,46 @@
 package ba.unsa.etf.rpr;
 
-public class KinoController {
+import javafx.beans.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+
+import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import static javafx.scene.layout.BorderPane.*;
+
+public class KinoController implements Initializable {
+    private ObservableList<String> names;
+    public Button closeButton;
+    public ListView<String> listView;
+
+
+    public void exit(ActionEvent actionEvent){
+        System.exit(0);
+    }
+    public void close(ActionEvent actionEvent){
+        Stage stage=(Stage)closeButton.getScene().getWindow();
+        stage.close();
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resourceBundle){
+        names= FXCollections.observableArrayList();
+        names.addAll("Horor");
+        listView.setItems(names);
+    }
+
+
+
+    public void add(ActionEvent actionEvent) {
+    }
 }
--- src/main/java/ba/unsa/etf/rpr/controllers/componets/DoubleButtonCellFactory.java
+++ src/main/java/ba/unsa/etf/rpr/controllers/componets/DoubleButtonCellFactory.java
@@ -1,4 +1,28 @@
 package ba.unsa.etf.rpr.controllers.componets;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.util.Callback;
 
-public class DoubleButtonCellFactory {
+public class DoubleButtonCellFactory<T> implements Callback<TableColumn<T, T>, TableCell<T, T>> {
+
+    private final EventHandler<ActionEvent> buttonOne;
+
+    private final EventHandler<ActionEvent> buttonTwo;
+
+    /**
+     *
+     * @param buttonOne - event handler for first button (Edit)
+     * @param buttonTwo - event handler for second button (Delete)
+     */
+    public DoubleButtonCellFactory(EventHandler<ActionEvent> buttonOne, EventHandler<ActionEvent> buttonTwo){
+        this.buttonOne = buttonOne;
+        this.buttonTwo = buttonTwo;
+    }
+
+    @Override
+    public TableCell<T, T> call(TableColumn<T, T> quoteObjectTableColumn) {
+        return new DoubleButtonTableCell<>(buttonOne, buttonTwo);
+    }
 }
